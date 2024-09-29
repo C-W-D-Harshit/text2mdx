d36940b6937a88a83a542629e55ec41b41fcd90c
Harshit Sharma <cwd.harshit911@gmail.com>
2024-09-29 14:37:55 +0530

    Refactor metadata URLs in robots.ts and sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://text2mdx.cleverdeveloper.in",
+      url: "https://text2mdx.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
