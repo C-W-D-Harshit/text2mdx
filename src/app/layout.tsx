9f4ee94045f84b6042a625d548c530883bd51420
Harshit Sharma <cwd.harshit911@gmail.com>
2024-09-30 08:23:10 +0530

    Add Google AdSense script to the head of the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2030617860941780"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
